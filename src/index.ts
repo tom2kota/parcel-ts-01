03fff9cdf8949e4c20b49314cd712ea51dec663e
Web Dev <tom2kota@gmail.com>
2021-04-23 18:11:38 +0300

    Add class Company

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-console.log('Hi there!')
-
+import {Company} from "./Company";
 import {User} from "./User";
 
 const user = new User()
+const company = new Company()
 
 console.log(user)
+console.log(company)
