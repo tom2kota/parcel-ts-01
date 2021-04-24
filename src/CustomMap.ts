16e46686bf05692d058c0737c9a072b4cb10a78f
Web Dev <tom2kota@gmail.com>
2021-04-24 18:58:29 +0300

    Add content to Marker method in a class CustomMap

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -8,6 +8,8 @@ interface Mappable {
         lat: number,
         lng: number
     }
+
+    markerContent(): string
 }
 
 export class CustomMap {
@@ -24,14 +26,21 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
         })
-    }
 
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            })
+
+            infoWindow.open(this.googleMap, marker)
+        })
+    }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,5 @@ const user = new User()
 const company = new Company()
 const customMap = new CustomMap('map')
 
-// console.log(user)
-// console.log(company)
-// console.log(customMap)
 customMap.addMarker(user)
 customMap.addMarker(company)
